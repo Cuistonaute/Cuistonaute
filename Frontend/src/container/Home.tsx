857f0a7b01aa3e4b72a9358352abeb8d2a53a7d4
mocquaischristopher <73651871+mocquaischristopher@users.noreply.github.com>
2022-02-06 07:32:24 +0100

    component card-image finish and page home finish with sass

--- Frontend/src/container/Home.tsx
+++ Frontend/src/container/Home.tsx
@@ -1,8 +1,28 @@
+import CardImage from "../components/CardImage";
+
 const Home = () => {
 
     return (
         <>
-            <h1>Hello Home!</h1>
+            <h1>Bienvenue sur Cuistonaute</h1>
+            <section className="card1">
+                <CardImage />
+                <CardImage />
+                <CardImage />
+                <CardImage />
+            </section>
+            <section className="card2">
+                <CardImage />
+                <CardImage />
+                <CardImage />
+                <CardImage />
+            </section>
+            <section className="card3">
+                <CardImage />
+                <CardImage />
+                <CardImage />
+                <CardImage />
+            </section>
         </>
     );
 }
