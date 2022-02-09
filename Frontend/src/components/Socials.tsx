7cca0a83becb8e607a2a816773798555b5502692
DannyPoizot <poizot.dan@gmail.com>
2022-02-09 15:06:54 +0100

    update path image

--- Frontend/src/components/Socials.tsx
+++ Frontend/src/components/Socials.tsx
@@ -6,7 +6,7 @@ const Socials = () => {
             <a className='fb social' href='#'><i className='fab fa-2x fa-facebook-square'></i></a>
             <a className='insta social' href="#"><i className='fab fa-2x fa-instagram-square'></i></a>
             <a className='twitter social' href='#'><i className='fab fa-2x fa-twitter-square'></i></a>    
-            <a className='gmail social' href='#'><img src='public/logoGmail.png' alt='Logo Gmail'/> </a>     
+            <a className='gmail social' href='#'><img src='../public/logoGmail.png' alt='Logo Gmail'/></a>     
         </>
     );
 };
